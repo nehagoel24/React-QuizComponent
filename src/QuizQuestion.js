5685658cbee2baa525230bd39714599957873edc
Goel, Neha D <neha.d.goel@accenture.com>
2021-03-02 15:36:10 +0530

    moduleError

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,9 +2,20 @@ import React, { Component } from "react";
 import QuizQuestionButton from "./QuizQuestionButton.js";
 
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      incorrectAnswer: false,
+    };
+  }
   handleClick(buttonText) {
     if (buttonText === this.props.quiz_question.answer) {
       this.props.showNextQuestionHandler();
+    }
+    if (this.state.incorrectAnswer === true) {
+      this.setState({ incorrectAnswer: false });
+    } else {
+      this.setState({ incorrectAnswer: true });
     }
   }
   render() {
@@ -29,6 +40,9 @@ class QuizQuestion extends Component {
               )}
             </ul>
           </section>
+          {this.state.incorrectAnswer ? (
+            <p className="error"> Sorry, that's not right</p>
+          ) : null}
         </main>
       </div>
     );
