4949780c0ed7af269649eccd0df22bb5479beb38
Goel, Neha D <neha.d.goel@accenture.com>
2021-03-02 14:52:06 +0530

    removedComment

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,8 +12,6 @@ class Quiz extends Component {
     };
   }
   render() {
-    // const isQuizEnd =
-    //   this.state.quiz_position - 1 === quizData.quiz_questions.length;
     const isQuizEnd = true;
     return (
       <div>
