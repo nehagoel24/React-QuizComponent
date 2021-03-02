5026507cf7033beb123daf9e10596d8d615f61b0
Goel, Neha D <neha.d.goel@accenture.com>
2021-03-02 14:43:39 +0530

    quizEnd

---
+++ src/QuizEnd.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class QuizQuestionButton extends Component {
+  render() {
+    return (
+      <div>
+        <p>Thanks for playing!</p>
+        <a href="">Reset Quiz</a>
+      </div>
+    );
+  }
+}
+
+export default QuizQuestionButton;
