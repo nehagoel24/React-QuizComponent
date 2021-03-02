d88be52b78351890f81135c57e35a246ded59bb6
Goel, Neha D <neha.d.goel@accenture.com>
2021-03-02 15:50:36 +0530

    true

--- src/Quiz.js
+++ src/Quiz.js
@@ -22,7 +22,7 @@ class Quiz extends Component {
     this.setState({ quiz_position: 1 });
   }
   render() {
-    const isQuizEnd = false;
+    const isQuizEnd = true;
     // this.state.quiz_position - 1 === quizData.quiz_questions.length;
     return (
       <div>
